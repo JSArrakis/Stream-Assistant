28dd6480ea7505612b84561f3575de302371b65a
Justin <jsarrakis.creddit@gmail.com>
2022-11-03 13:07:54 -0700

    Initial Commit
    Initial commit to repository.
    
    Currently working on buffer constructors and next up is the cadence engine

---
+++ index.js
@@ -0,0 +1,44 @@
+'use strict';
+const { dir } = require("console");
+const fs = require("fs");
+const config = require("./config.json");
+const dirMan = require("./lib/directoryManager.js");
+const saCommander = require("./lib/saCommander.js");
+const streamConstructor = require("./lib/streamConstructor.js")
+
+const main = async () => {
+	//Create Commandline Options
+	var options = saCommander.parseCommandLineArgs();
+	let playlistFileName = config.PlaylistName;
+	let destinationDirectory = options.test ? config.TestFolder : config.DestinationFolder; //Directory in which to place all files created by Stream Assistant 
+
+	//If result comes back false, do nothing, otherwise proceed.
+	if (await dirMan.manageDestinationDirectory(config, destinationDirectory, options)) {
+
+		//TODO: Create progression option for non collection block shows
+		//TODO: Optional: I need to know what era the playlist shows represent (based on cross section of metadata stored on each show object)
+		//TODO: Optional: I need to know the weight of number of episodes of each series for proper number of episodes per stream (metadata makes this easier)
+		let stream = streamConstructor.constructStream(config, destinationDirectory, options);
+
+		dirMan.createStreamFile(destinationDirectory, playlistFileName, stream);
+
+		//TODO: Create output text (possibly in file) ready to copy and paste into Discord for easy stream announcements
+
+		saCommander.executeStream(config, destinationDirectory, playlistFileName);
+
+		dirMan.createProgressionFile(destinationDirectory, progression); await fs.promises.writeFile(destinationDirectory + 'progression.json', JSON.stringify(progression), (err) => {
+			if (err) throw err;
+			console.log('Progression File was created successfully.');
+		});
+
+		if (options.test) {
+			console.log("Hope you meant to have testing enabled. Womp womp.");
+		}
+	} else {
+		console.log("STREAM ASSISTANT DIDN'T DO ANYTHING! Assistant has already been ran for the current date.");
+		console.log("If this is a mistake or you screwed up, guess you have to fix it manually. -- Love your Past Self");
+	}
+
+}
+
+main();
