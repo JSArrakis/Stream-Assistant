cfdb4e28dbd3dec3604bef52762b63a11dc54e1a
Justin <jsarrakis.creddit@gmail.com>
2023-02-02 12:18:08 -0800

    removed stream execution functionality from saCommander, and refactored

--- index.ts
+++ index.ts
@@ -1,4 +1,7 @@
 import config from "./config.json";
+import { parseCommandLineArgs } from "./lib/saCommander";
+import { executeStream } from "./lib/streamExecutor";
+import * as dirMan from "./lib/directoryManager"
 
 interface Config {
 	vlcLocation: string;
@@ -8,7 +11,26 @@ interface Config {
 	playlistName: string;
 }
 
+const typedConfig: Config = config;
+
 async function main(): Promise<void> {
+
+
+	//Get user selection
+	let options = parseCommandLineArgs();
+
+	//Get progression
+	let progression = [];
+	if (dirMan.directoryExists(config.destinationFolder + "progression.json")) {
+		progression = JSON.parse(dirMan.readFile(config.destinationFolder + "progression.json"));
+	}
+
+	//Create Stream
+
+	//Create Stream File
+
+	//Execute Stream
+	executeStream(config.vlcLocation, config.destinationFolder, config.playlistName);
 
 }
 
